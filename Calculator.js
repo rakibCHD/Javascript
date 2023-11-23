eb6c89ae6f60843f84f242c00dc074cc36feae7a
Rakib Chowdhury <rakibchowdhury2700@gmail.com>
2023-11-23 18:18:55 +0600

    first commit

---
+++ Calculator.js
@@ -0,0 +1,24 @@
+
+console.log('-------Simple calculator------');
+
+const prompt = require("prompt-sync")();
+const number1 = parseFloat(prompt('Enter first number: '));
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+console.log(`${number1} ${operator} ${number2} = ${result}`);
